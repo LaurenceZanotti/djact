a1e6fa2d1c7e31ce779f5dd715fb8c6455fcd647
LaurenceZanotti <lau_zanotti@hotmail.com>
2021-09-30 16:50:45 -0300

    React, webpack and babel basic setup

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <main className="w-50 m-auto">
+            <h1 className="text-center">Hello world!</h1>
+        </main>
+    )
+}

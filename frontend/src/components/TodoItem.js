c5dc9179b7506c6b2447bfbb38896c56ce7eb2b0
LaurenceZanotti <lau_zanotti@hotmail.com>
2021-10-02 17:56:34 -0300

    ToDo app phase 5

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -4,7 +4,7 @@ export default function TodoItem(props) {
     const {text, completed} = props
     return (
         <div className="todoItem">
-            <input type="checkbox" name="" id="" checked={completed}/>
+            <input type="checkbox" name="" id="" checked={completed} onChange={() => console.log('Checkbox clicked!')}/>
             <p>{text}</p>
         </div>
     )

7efbfcf7ae64cb05041864ef0a6c00a82c39ba83
LaurenceZanotti <lau_zanotti@hotmail.com>
2021-10-02 17:48:28 -0300

    ToDo app phase 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Todo from './components/TodoItem'
 import todosData from './todosData'
 
 
-export default function App() {
-    const TodoList = todosData.map(todo => <Todo key={todo.id} text={todo.text} completed={todo.completed}/>)
-
-    return (
-        <main className="w-50 m-auto">
-            <h1>ToDo App</h1>
-            {TodoList}
-        </main>
-    )
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todoList: todosData.map(todo => <Todo key={todo.id} text={todo.text} completed={todo.completed}/>)
+        }
+    }
+    render() {
+        return (
+            <main className="w-50 m-auto">
+                <h1>ToDo App</h1>
+                {this.state.todoList}
+            </main>
+        )
+    }
 }

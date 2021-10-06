8ec6b5171cfaf8d18a023fedf555d8fa7647dc60
LaurenceZanotti <lau_zanotti@hotmail.com>
2021-10-05 21:34:22 -0300

    Fetch simples para API de terceiros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export default function App() {
-    return (
-        <main className="w-50 m-auto">
-            <h1 className="text-center">Hello world!</h1>
-        </main>
-    )
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            // Todo: Add loading status
+            bored: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://www.boredapi.com/api/activity")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({bored: data})
+            })
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <main className="w-50 m-auto">
+                <h1 className="text-center m-3">Nothing to do?</h1>
+                <p>{this.state.bored.activity}</p>
+            </main>
+        )
+    }
+    
 }
